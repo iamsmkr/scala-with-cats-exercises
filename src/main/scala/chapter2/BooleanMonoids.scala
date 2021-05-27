e465c1b5451f4e7e7d228c47d0567ae0d005945b
iamsmkr <mailtoshivamk@gmail.com>
2021-05-27 17:11:10 +0000

    boolean monoids

---
+++ src/main/scala/chapter2/BooleanMonoids.scala
@@ -0,0 +1,34 @@
+package chapter2
+
+import cats._
+
+object BooleanMonoids {
+
+  implicit val booleanAndMonoid: Monoid[Boolean] =
+    new Monoid[Boolean] {
+      override def empty: Boolean = true
+
+      override def combine(x: Boolean, y: Boolean): Boolean = x && y
+    }
+
+  implicit val booleanOrMonoid: Monoid[Boolean] =
+    new Monoid[Boolean] {
+      override def empty: Boolean = false
+
+      override def combine(x: Boolean, y: Boolean): Boolean = x || y
+    }
+
+  implicit val booleanEitherMonoid: Monoid[Boolean] =
+    new Monoid[Boolean] {
+      override def empty: Boolean = false
+
+      override def combine(x: Boolean, y: Boolean): Boolean = (a && !b) || (!a && b)
+    }
+
+  implicit val booleanXnorMethod: Monoid[Boolean] =
+    new Monoid[Boolean] {
+      override def empty: Boolean = true
+
+      override def combine(x: Boolean, y: Boolean): Boolean = (!a || b) && (a || !b)
+    }
+}
